4a29c3f446bd024a14c91f52daf1dcf1559cea04
KyungsooLee <jmtkd9196@gmail.com>
2023-07-04 00:16:32 +0900

    [feature/MonthlyStoryCollection]feat: Implement MonthlyStoryCollection & Add String+Extension

---
+++ Owori/Owori/View/Story/Album/MonthlyStoryCollection.swift
@@ -0,0 +1,49 @@
+//
+//  MonthlyStoryCollection.swift
+//  Owori
+//
+//  Created by Kyungsoo Lee on 2023/07/03.
+//
+
+import SwiftUI
+
+struct MonthlyStoryCollection: View {
+    // MARK: PROPERTIES
+    
+    /// - [임시] 모델의 이미지를 대신하기 위한 변수
+    /// - 실제 이미지가 들어오면 없어질 예정
+    private let images = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    
+    /// - 표시할 열의 수
+    private let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    
+    // MARK: BODY
+    var body: some View {
+        VStack(alignment: .leading) {
+            // 날짜
+            Text("yyyy.MM".stringFromDate())
+                .font(.title3)
+                .bold()
+            
+            // Album Collection View
+            LazyVGrid(columns: columns, spacing: 16) {
+                ForEach(images[0 ... 5], id: \.self) { imageName in
+                    DailyStoryImageCell()
+                }
+            }
+        }
+    }
+}
+
+
+// MARK: PREVIEWS
+struct MonthlyStoryCollection_Previews: PreviewProvider {
+    static var previews: some View {
+        MonthlyStoryCollection()
+    }
+}
