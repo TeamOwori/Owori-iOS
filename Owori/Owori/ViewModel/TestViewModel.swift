3bb9a64813a88f5d189159a33e5a723e0ac4ff30
KyungsooLee <jmtkd9196@gmail.com>
2023-07-03 19:40:10 +0900

    [feature/refactor]refactor: Add TestFile

---
+++ Owori/Owori/ViewModel/TestViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  TestViewModel.swift
+//  Owori
+//
+//  Created by Kyungsoo Lee on 2023/07/03.
+//
+
+import SwiftUI
+
+struct TestViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        TestViewModel()
+    }
+}
