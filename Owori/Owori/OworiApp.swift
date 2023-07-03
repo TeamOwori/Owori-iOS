a92ed2277cb526e87b0a001c005cf8e1aa19c41a
KyungsooLee <jmtkd9196@gmail.com>
2023-07-03 18:05:00 +0900

    [main]feat: Create init project

---
+++ Owori/Owori/OworiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OworiApp.swift
+//  Owori
+//
+//  Created by Kyungsoo Lee on 2023/07/03.
+//
+
+import SwiftUI
+
+@main
+struct OworiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
