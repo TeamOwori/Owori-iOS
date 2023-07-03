19f82fdbfe461a73c664bae319919e2bb378ee40
KyungsooLee <jmtkd9196@gmail.com>
2023-07-04 01:17:31 +0900

    [feature/Story]feat: Implement StoryAlbumView

---
+++ Owori/Owori/View/Story/Album/StoryAlbumView.swift
@@ -0,0 +1,31 @@
+//
+//  StoryAlbumView.swift
+//  Owori
+//
+//  Created by Kyungsoo Lee on 2023/07/04.
+//
+
+import SwiftUI
+
+struct StoryAlbumView: View {
+    // MARK: PROPERTIES
+    /// - [임시] 컬렉션의 갯수
+    /// - 실제 데이터 들어오면 없어질 예정
+    private var collections = ["1", "2", "3", "4"]
+    
+    // MARK: BODY
+    var body: some View {
+        ScrollView {
+            ForEach(collections, id: \.self) { collection in
+                MonthlyStoryCollection()
+            }
+        }
+    }
+}
+
+// MARK: PREVIEWS
+struct StoryAlbumView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoryAlbumView()
+    }
+}
