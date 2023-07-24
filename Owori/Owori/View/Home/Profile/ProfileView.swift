01783dc035122a49c40cafdb3fd08fb21625c616
zxcvb2002 <jbyjby3743@naver.com>
2023-07-25 03:33:55 +0900

    [feature/Home]feat: Import DefaultImage

--- Owori/Owori/View/Home/Profile/ProfileView.swift
+++ Owori/Owori/View/Home/Profile/ProfileView.swift
@@ -26,31 +26,11 @@ struct ProfileView: View {
                     ProfileImageWithBadge()
                 }
             }
-            .frame(height: UIScreen.main.bounds.height * 0.1)
-            .background(Color.red)
         }
-        .padding(EdgeInsets(top: 0, leading: UIScreen.main.bounds.width * 0.05, bottom: 0, trailing: 0))
-        .frame(height: UIScreen.main.bounds.height * 0.1) //
+        .padding(EdgeInsets(top: 0, leading: UIScreen.main.bounds.width * 0.05, bottom: 0, trailing: UIScreen.main.bounds.width * 0.05))
+        .frame(height: UIScreen.main.bounds.height * 0.125)
     }
 }
-
-//struct ProfileView: View {
-//    var body: some View {
-////        NavigationStack {
-//            ZStack {
-//                Color.oworiMainColor
-//                HStack(spacing: 16) {
-//                    ForEach(1...3, id: \.self) { _ in
-//                        ProfileImageWithBadge()
-//                    }
-//                }
-//                .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
-//                .frame(width: UIScreen.main.bounds.width * 0.95, alignment: .leading)
-//            }
-//            .frame(height: UIScreen.main.bounds.height * 0.05)
-//        }
-////    }
-//}
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
