83ef03ea7fd43ea534cd4103861f82d20ddb563b
KyungsooLee <jmtkd9196@gmail.com>
2023-07-26 15:12:16 +0900

    [feature/View]feat: Modify Info.plist

--- Owori/Owori/ViewModel/UserViewModel.swift
+++ Owori/Owori/ViewModel/UserViewModel.swift
@@ -9,11 +9,12 @@ import SwiftUI
 
 fileprivate enum OworiAPI {
     static let scheme = "http"
-    static let host = "localhost:8080"
+    static let host = "owori.store"
     
     enum Path: String {
-        case refreshingToken = "api/v1/auth/refresh"
-        case initUser = "api/v1/members/details"
+        case joinMember = "/api/v1/members"
+        case refreshingToken = "/api/v1/auth/refresh"
+        case initUser = "/api/v1/members/details"
     }
 }
 
@@ -29,17 +30,17 @@ class UserViewModel: ObservableObject {
         
         // 요청을 보낼 API의 url 설정
         // 배포 후 url 설정
-        //        var urlComponents = URLComponents()
-        //        urlComponents.scheme = OworiAPI.scheme
-        //        urlComponents.host = OworiAPI.host
-        //        urlComponents.path = OworiAPI.Path.joinMember.rawValue
-        //        guard let url = urlComponents.url else {
-        //            print("Error: cannot create URL")
-        //            return
-        //        }
+        var url = URLComponents()
+        url.scheme = OworiAPI.scheme
+        url.host = OworiAPI.host
+        url.path = OworiAPI.Path.joinMember.rawValue
+        guard let url = url.url else {
+            print("Error: cannot create URL")
+            return
+        }
         
         // 배포 이전 고정 url 설정 (추후 삭제 예정)
-        let url = URL(string: "http://localhost:8080/api/v1/members")!
+        //        let url = URL(string: "http://localhost:8080/api/v1/members")!
         
         // url 테스트 log
         print("[joinMember url Log] : \(url)")
@@ -196,7 +197,7 @@ class UserViewModel: ObservableObject {
     // 멤버 프로필 업데이트
     func updateProfile(userInfo: [String: Any]) {
         guard let sendData = try? JSONSerialization.data(withJSONObject: userInfo, options: []) else { return }
-
+        
         // 요청을 보낼 API의 url 설정
         // 배포 후 url 설정
         //        var urlComponents = URLComponents()
@@ -207,13 +208,13 @@ class UserViewModel: ObservableObject {
         //            print("Error: cannot create URL")
         //            return
         //        }
-
+        
         // 배포 이전 고정 url 설정 (추후 삭제 예정)
         let url = URL(string: "http://localhost:8080/api/v1/members/profile")!
-
+        
         // url 테스트 log
         print("[Update Profile User url Log]: \(url)")
-
+        
         // urlRequeset에 함께 담을 header, body 설정
         var urlRequest = URLRequest(url: url)
         urlRequest.httpMethod = "POST"
@@ -221,7 +222,7 @@ class UserViewModel: ObservableObject {
         urlRequest.setValue(self.user.member_id, forHTTPHeaderField: "memberId")
         urlRequest.setValue("Bearer " + (self.user.jwt_token?.access_token)!, forHTTPHeaderField: "Authorization")
         urlRequest.httpBody = sendData
-
+        
         // 요청
         URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
             guard error == nil else {
@@ -233,12 +234,12 @@ class UserViewModel: ObservableObject {
                 print("Error: Did not receive data")
                 return
             }
-
+            
             guard let response = response as? HTTPURLResponse, (200 ..< 300) ~= response.statusCode else {
                 print("Error: HTTP request failed")
                 return
             }
-
+            
         }.resume()
     }
     
@@ -432,10 +433,10 @@ class UserViewModel: ObservableObject {
                 // JSON 데이터를 파싱하여 User 구조체에 할당
                 do {
                     let decoder = JSONDecoder()
-//                    self?.user.member_profile = try decoder.decode(User.Profile.self, from: data)
+                    //                    self?.user.member_profile = try decoder.decode(User.Profile.self, from: data)
                     
                     // User 구조체에 할당된 데이터 사용 (테스트 log)
-//                    print("Member Profile: \(String(describing: self?.user.member_profile))")
+                    //                    print("Member Profile: \(String(describing: self?.user.member_profile))")
                 } catch {
                     print("Error: Failed to parse JSON data - \(error)")
                 }
@@ -485,10 +486,10 @@ class UserViewModel: ObservableObject {
                 print("Error: response error")
                 return
             }
-//            guard let jsonDictionary = try? JSONSerialization.jsonObject(with: Data(data), options: []) as? [String: Any] else {
-//                print("Error: convert failed json to dictionary")
-//                return
-//            }
+            //            guard let jsonDictionary = try? JSONSerialization.jsonObject(with: Data(data), options: []) as? [String: Any] else {
+            //                print("Error: convert failed json to dictionary")
+            //                return
+            //            }
             guard let response = response as? HTTPURLResponse, (200 ..< 300) ~= response.statusCode else {
                 print("Error: HTTP request failed")
                 return
